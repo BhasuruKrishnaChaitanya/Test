ce67a83027879929d5bb3bb038c05afdcd2da00f
Goutham <gouthamkallem@gmail.com>
2020-12-09 17:45:12 +0530

    Changes

--- test.java
+++ test.java
@@ -6,9 +6,35 @@ import org.apache.poi.ss.usermodel.Row;
 import org.apache.poi.xssf.usermodel.XSSFSheet;  
 import org.apache.poi.xssf.usermodel.XSSFWorkbook;  
 public class test{
+	public static XSSFSheet sheet;
+	public static int getCount(XSSFSheet sheet) {
+		Iterator<Row> itr = sheet.iterator();    //iterating over excel file
+        int cnt = 0;
+        while (itr.hasNext()){
+            Row row = itr.next();
+            Iterator<Cell> cellIterator = row.cellIterator();   //iterating over each column
+            while (cellIterator.hasNext()){
+                Cell cell = cellIterator.next();
+                switch (cell.getCellType()){
+                    case Cell.CELL_TYPE_STRING:    //field that represents string cell type
+                    System.out.print(cell.getStringCellValue() + "\t");  //Operation
+                    break;
+                    case Cell.CELL_TYPE_NUMERIC:    //field that represents number cell type
+                    System.out.print(cell.getNumericCellValue() + "\t");  //Operation
+                    break;
+                    default:
+                }
+            } 
+            System.out.println(""); 
+            cnt++;//Operation
+        }
+		return cnt;
+	}
+	
+	
     public static void main(String[] args){
         try{
-            File file = new File("C:\\demo\\employee.xlsx");   //creating a new file instance
+            File file = new File("StudentsEnrolledByWeek.xlsx");   //creating a new file instance
             FileInputStream fis = new FileInputStream(file);   //obtaining bytes from the file
             //creating Workbook instance that refers to .xlsx file
             XSSFWorkbook wb = new XSSFWorkbook(fis);
@@ -22,20 +48,24 @@ public class test{
                     Cell cell = cellIterator.next();
                     switch (cell.getCellType()){
                         case Cell.CELL_TYPE_STRING:    //field that represents string cell type
-                        System.out.print(cell.getStringCellValue() + "\t\t\t");  //Operation
+                        System.out.print(cell.getStringCellValue() + "\t");  //Operation
                         break;
                         case Cell.CELL_TYPE_NUMERIC:    //field that represents number cell type
-                        System.out.print(cell.getNumericCellValue() + "\t\t\t");  //Operation
+                        System.out.print(cell.getNumericCellValue() + "\t");  //Operation
                         break;
                         default:
                     }
                 } 
-                /*System.out.println("");*/ cnt++;//Operation
+                System.out.println(""); 
+                cnt++;//Operation
             }
             System.out.println(cnt);
         }
         catch(Exception e){
             e.printStackTrace();
         }  
+        getCount(sheet);
+
     }
+    
 }
